62b27836d6d35d9875506d7ded631a24063e72af
Vidal721 <aprich1109@gmail.com>
2024-10-25 11:19:58 -0500

    Update setup.js

--- 935scouting/interface/setup.js
+++ 935scouting/interface/setup.js
@@ -1,1 +1,6 @@
-js
+var password = prompt("What would you like the password do be?")
+var username = prompt("What would you like your username to be")
+var confirm = confirm("This will only be saved to this device")
+
+
+localstorage.setitem(password)
